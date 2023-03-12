2d4fdb6728cb0d86cdb70620a41a076b9cadc2aa
Sardor Tursunboyev <english.programmer.9595@gmail.com>
2023-03-12 11:41:20 +0500

    Prepare router 6

--- src/index.jsx
+++ src/index.jsx
@@ -1,9 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { ReactDomSix } from "./router_6";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-  
+    <ReactDomSix />
   </React.StrictMode>
 );
---
+++ src/router_6/index.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import { BrowserRouter as Router } from "react-router-dom";
+
+export const ReactDomSix = () => {
+  return (
+    <div>
+      <Router>Hiii</Router>
+    </div>
+  );
+};
