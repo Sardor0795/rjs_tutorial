7e1c699a72051575e8031482fd183397eb6f9384
Sardor Tursunboyev <english.programmer.9595@gmail.com>
2023-03-12 20:10:59 +0500

    Main style styled

--- src/index.jsx
+++ src/index.jsx
@@ -1,15 +1,18 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter as Router } from "react-router-dom";
-import "./index.css";
+import "./style.jsx";
 
 // import { ReactDomSix } from "./router_6";
-// import { GreenShop } from "./router_6/greenshop";
+import { GreenShop } from "./router_6/greenshop";
+import { Div } from "./style.jsx";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <Router>
-    {/* <ReactDomSix /> */}
-    {/* <GreenShop/> */}
-  </Router>
+  <Div>
+    <Router>
+      {/* <ReactDomSix /> */}
+      <GreenShop />
+    </Router>
+  </Div>
 );
