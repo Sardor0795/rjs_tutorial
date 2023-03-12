b9ba2837ae45cb5c556350e24aa6cc4ee2fdf179
Sardor Tursunboyev <english.programmer.9595@gmail.com>
2023-03-12 14:20:36 +0500

    prepare to green shop project

--- src/index.jsx
+++ src/index.jsx
@@ -1,15 +1,17 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { ReactDomSix } from "./router_6";
+// import { ReactDomSix } from "./router_6";
 import "./index.css";
 
 import { BrowserRouter as Router } from "react-router-dom";
+import { GreenShop } from "./router_6/greenshop";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <Router>
-      <ReactDomSix />
+      {/* <ReactDomSix /> */}
+      <GreenShop/>
     </Router>
   </React.StrictMode>
 );
---
+++ src/router_6/greenshop/index.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export const GreenShop = () => {
+  return (
+    <div>GreenShop</div>
+  )
+}
