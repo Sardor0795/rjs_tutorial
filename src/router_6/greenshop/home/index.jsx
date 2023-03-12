1fd7668429a05ed2af547020f63b0c15a9e86998
Sardor Tursunboyev <english.programmer.9595@gmail.com>
2023-03-12 23:43:21 +0500

    Add gShop search

--- src/router_6/greenshop/home/index.jsx
+++ src/router_6/greenshop/home/index.jsx
@@ -1,12 +1,21 @@
 import React from "react";
+import { NavLink } from "react-router-dom";
+import { categories } from "../mock/category";
 
 import "./style.css";
 
 export const Home = () => {
   return (
-    <div className="home">
+    <div className="home home">
       <div className="category">
         <h2>Categories</h2>
+        {categories.map((v) => {
+          return (
+            <NavLink to={"/home/cat"}>
+              <div>{v.title}</div>
+            </NavLink>
+          );
+        })}
       </div>
       <div className="cards"></div>
     </div>
---
+++ src/router_6/greenshop/mock/category.jsx
@@ -0,0 +1,5 @@
+export const categories = [
+  { id: 1, title: "Plant" },
+  { id: 2, title: "Tree" },
+  { id: 3, title: "Leaf" },
+];
