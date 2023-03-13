8238e4241f5219d3d101b76302922c90c3f1f70d
Sardor Tursunboyev <english.programmer.9595@gmail.com>
2023-03-13 20:27:56 +0500

    prepare to 3rd lesson

--- src/index.jsx
+++ src/index.jsx
@@ -1,18 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { BrowserRouter as Router } from "react-router-dom";
 import "./style.jsx";
 
-// import { ReactDomSix } from "./router_6";
-import { GreenShop } from "./router_6/greenshop";
 import { Div } from "./style.jsx";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(
-  <Div>
-    <Router>
-      {/* <ReactDomSix /> */}
-      <GreenShop />
-    </Router>
-  </Div>
-);
+root.render(<Div></Div>);
