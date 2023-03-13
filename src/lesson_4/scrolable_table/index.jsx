a84b0a8019db07f70a4219c37be26bc1b5b89cf5
Sardor Tursunboyev <english.programmer.9595@gmail.com>
2023-03-13 21:52:51 +0500

    Prepare table

--- src/lesson_4/scrolable_table/index.jsx
+++ src/lesson_4/scrolable_table/index.jsx
@@ -1,13 +1,27 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 
 class Table extends Component {
-    render() {
-        return (
-            <div>
-                Table
-            </div>
-        );
-    }
+  render() {
+    return (
+      <div className="tableWrapper">
+        <table>
+          <thead>
+            <tr>
+              <th>Id</th>
+              <th>Name</th>
+              <th>Age</th>
+              <th>Address</th>
+              <th>Status</th>
+              <th>Nickname</th>
+              <th>Univ</th>
+              <th>Job</th>
+              <th>Edit</th>
+            </tr>
+          </thead>
+        </table>
+      </div>
+    );
+  }
 }
 
 export default Table;
