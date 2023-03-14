95c39211c27de4b4eddc36c11fe405d9c101c827
Sardor Tursunboyev <english.programmer.9595@gmail.com>
2023-03-14 16:54:56 +0500

    Make mock data

---
+++ src/mock/students.jsx
@@ -0,0 +1,8 @@
+let students = [
+  { id: 1, name: "Jim", surname: "Dale" },
+  { id: 2, name: "Ann", surname: "Adkinson" },
+  { id: 3, name: "Polly", surname: "Dale" },
+  { id: 4, name: "Kurt", surname: "Jackson" },
+  { id: 5, name: "John", surname: "Dale" },
+  { id: 6, name: "Bill", surname: "Adkinson" },
+];
