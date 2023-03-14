af6ad18bfec5324717d8a14a326facbd8f74aad1
Sardor Tursunboyev <english.programmer.9595@gmail.com>
2023-03-14 17:51:36 +0500

    Function call, Multi state, CRUD - Read

--- src/lesson_4/index.jsx
+++ src/lesson_4/index.jsx
@@ -17,6 +17,10 @@ class LessonFour extends Component {
       console.log(e.target.value);
       console.log(e.target.name);
     };
+    const onFilter = (e) => {
+      let fltrd = students.filter((v) => v.name.toLowerCase().includes(e.target.value.toLowerCase()));
+      this.setState({data: fltrd})
+    };
     return (
       <div
         style={{
@@ -44,9 +48,11 @@ class LessonFour extends Component {
           type="text"
           placeholder="Surname"
         />
+        <hr style={{ widows: "100%" }} />
+        <input onChange={onFilter} type="text" placeholder="Filter" />
         {this.state.data.map((v) => {
           return (
-            <h1>
+            <h1 key={v.id}>
               Id: {v.id} Name: {v.name} Surname: {v.surname}
             </h1>
           );
