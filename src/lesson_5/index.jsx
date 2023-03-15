e490a2ad3a3a6ceccedf31539811e4b856b94b57
Sardor Tursunboyev <english.programmer.9595@gmail.com>
2023-03-15 14:15:17 +0500

    Distructure

--- src/lesson_5/index.jsx
+++ src/lesson_5/index.jsx
@@ -38,24 +38,26 @@ class LessonFive extends Component {
               </tr>
             </thead>
             <tbody>
-              {this.state.data.map((v) => {
-                return (
-                  <tr>
-                    <td>{v.id}</td>
-                    <td>{v.name}</td>
-                    <td>{v.age}</td>
-                    <td>{v.address}</td>
-                    <td>{v.status}</td>
-                    <td>{v.nickname}</td>
-                    <td>{v.univ}</td>
-                    <td>{v.job}</td>
-                    <td>
-                      <button className="btn">Edit</button>
-                      <button className="btn">Dell</button>
-                    </td>
-                  </tr>
-                );
-              })}
+              {this.state.data.map(
+                ({ id, name, age, address, status, nickname, univ, job }) => {
+                  return (
+                    <tr key={id}>
+                      <td>{id}</td>
+                      <td>{name}</td>
+                      <td>{age}</td>
+                      <td>{address}</td>
+                      <td>{status}</td>
+                      <td>{nickname}</td>
+                      <td>{univ}</td>
+                      <td>{job}</td>
+                      <td>
+                        <button className="btn">Edit</button>
+                        <button className="btn">Dell</button>
+                      </td>
+                    </tr>
+                  );
+                }
+              )}
             </tbody>
           </table>
         </div>
