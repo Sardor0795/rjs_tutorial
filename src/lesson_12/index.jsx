c261b1becf49e366000bac878ef952fba43424fd
Sardor Tursunboyev <english.programmer.9595@gmail.com>
2023-03-30 19:34:44 +0500

    useReducer

--- src/lesson_12/index.jsx
+++ src/lesson_12/index.jsx
@@ -1,7 +1,43 @@
-import React from 'react'
+import React from "react";
+import { useState } from "react";
+import { useReducer } from "react";
 
 export const LessonTwelve = () => {
+  const reducer = (state, action) => {
+    switch (action.type) {
+      case "plus":
+        return state + 1;
+      case "minus":
+        return state - 1;
+      case "byAmount":
+        return state + action.payLoad;
+      default:
+        return state;
+    }
+  };
+
+  const [counter, dispach] = useReducer(reducer, 0);
+  const [amount, setAmount] = useState(0);
+
   return (
-    <div>LessonTwelve</div>
-  )
-}
+    <div>
+      <span>{counter}</span>
+      <button onClick={() => dispach({ type: "plus" })}>+</button>
+      <button onClick={() => dispach({ type: "minus" })}>-</button>
+      <button onClick={() => dispach({ type: "byAmount", payLoad: 10 })}>
+        10
+      </button>
+      <button onClick={() => dispach({ type: "byAmount", payLoad: 70 })}>
+        70
+      </button>
+      <button onClick={() => dispach({ type: "byAmount", payLoad: +amount })}>
+        selected
+      </button>
+      <select onChange={(e) => setAmount(e.target.value)}>
+        <option value="1">1</option>
+        <option value="5">5</option>
+        <option value="10">10</option>
+      </select>
+    </div>
+  );
+};
