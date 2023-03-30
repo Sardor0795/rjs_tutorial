c7bded7425da4fc22e614dca8b48e9eeb7547457
Sardor Tursunboyev <english.programmer.9595@gmail.com>
2023-03-30 12:03:59 +0500

    prepare 12-lesson folder, files

--- src/index.jsx
+++ src/index.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import ClassComp from "./lesson_11/classComp.jsx";
+import { LessonTwelve } from "./lesson_12/index.jsx";
 import "./style.jsx";
 
 import { Div } from "./style.jsx";
@@ -8,6 +8,6 @@ import { Div } from "./style.jsx";
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <Div>
-    <ClassComp/>
+    <LessonTwelve/>
   </Div>
 );
