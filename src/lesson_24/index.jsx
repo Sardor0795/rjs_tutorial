2c42e9244c8c54e7fbfd5dcf78fd4bb0d1cce6d6
Sardor Tursunboyev <english.programmer.9595@gmail.com>
2023-04-02 09:53:38 +0500

    lazy load

--- src/lesson_24/index.jsx
+++ src/lesson_24/index.jsx
@@ -1,20 +1,26 @@
-import React, { Suspense } from "react";
-import { NavLink, Route } from "react-router-dom";
-import { Comp1 } from "./comp1";
+import React from "react";
+// import { NavLink, Route, Routes } from "react-router-dom";
+// import { Comp1 } from "./comp1";
+// import { Comp2 } from "./comp2";
 
-const Comp2 = React.lazy(() => import("./comp2"));
+// const Comp2 = React.lazy(() => import("./comp2"));
 
 export const LazyLoad = () => {
   return (
     <div>
-      <NavLink to={"/comp1"}>Comp1</NavLink>
+      {/* <NavLink to={"/comp1"}>Comp1</NavLink>
       <NavLink to={"/comp2"}>Comp2</NavLink>
-
-      <Route path={"/comp1"} component={Comp1} />
-      <Route
-        path={"/comp2"}
-        component={<Suspense fallback={<h1>Loading...</h1>}>{Comp2}</Suspense>}
-      />
+      <Routes>
+        <Route path={"/comp1"} element={<Comp1 />} />
+        <Route
+          path="/comp2"
+          element={
+            <React.Suspense fallback={<h1>Loading...</h1>}>
+              <Comp2 />
+            </React.Suspense>
+          }
+        />
+      </Routes> */}
     </div>
   );
 };
---
+++ src/lesson_24/modal.jsx
@@ -0,0 +1,8 @@
+// import React from "react";
+
+// export const Modal = () => {
+//   return ReactDOM.createPortal(
+//     <div>Modal</div>,
+//     document.getElementById("portal")
+//   );
+// };
